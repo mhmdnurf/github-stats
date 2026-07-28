69375bf27004060b8e31b68cf1757a2e2ddfb8e9
mhmdnurf <mhmd.nurfat@gmail.com>
2026-07-28 14:36:35 +0700

    feat: integrate Vercel Analytics into the application and update package dependencies

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,31 +1,35 @@
-import './App.css'
-import { ColorModeProvider } from './lib/color-mode'
-import { Header } from './components/sections/Header'
-import { Hero } from './components/sections/Hero'
-import { Features } from './components/sections/Features'
-import { QuickStart } from './components/sections/QuickStart'
-import { Usage } from './components/sections/Usage'
-import { ThemesGallery } from './components/sections/ThemesGallery'
-import { ApiReference } from './components/sections/ApiReference'
-import { Deployment } from './components/sections/Deployment'
-import { Footer } from './components/sections/Footer'
+import "./App.css";
+import { ColorModeProvider } from "./lib/color-mode";
+import { Header } from "./components/sections/Header";
+import { Hero } from "./components/sections/Hero";
+import { Features } from "./components/sections/Features";
+import { QuickStart } from "./components/sections/QuickStart";
+import { Usage } from "./components/sections/Usage";
+import { ThemesGallery } from "./components/sections/ThemesGallery";
+import { ApiReference } from "./components/sections/ApiReference";
+import { Deployment } from "./components/sections/Deployment";
+import { Footer } from "./components/sections/Footer";
+import { Analytics } from "@vercel/analytics/next";
 
 function App() {
-  return (
-    <ColorModeProvider>
-      <Header />
-      <main>
-        <Hero />
-        <Features />
-        <QuickStart />
-        <Usage />
-        <ThemesGallery />
-        <ApiReference />
-        <Deployment />
-      </main>
-      <Footer />
-    </ColorModeProvider>
-  )
+	return (
+		<>
+			<Analytics />
+			<ColorModeProvider>
+				<Header />
+				<main>
+					<Hero />
+					<Features />
+					<QuickStart />
+					<Usage />
+					<ThemesGallery />
+					<ApiReference />
+					<Deployment />
+				</main>
+				<Footer />
+			</ColorModeProvider>
+		</>
+	);
 }
 
-export default App
+export default App;
