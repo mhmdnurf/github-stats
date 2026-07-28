a266ca78a1eb9e659bd210b453f05658d7af4d0e
mhmdnurf <mhmd.nurfat@gmail.com>
2026-07-28 11:39:06 +0700

    grant deployer access to Cloud Run image

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -201,6 +201,14 @@ resource "google_project_iam_member" "builder_artifact_writer" {
   member  = "serviceAccount:${google_service_account.builder.email}"
 }
 
+resource "google_artifact_registry_repository_iam_member" "deployer_artifact_reader" {
+  project    = var.project_id
+  location   = google_artifact_registry_repository.images.location
+  repository = google_artifact_registry_repository.images.repository_id
+  role       = "roles/artifactregistry.reader"
+  member     = "serviceAccount:${google_service_account.deployer.email}"
+}
+
 resource "google_service_account_iam_member" "deployer_runtime_user" {
   service_account_id = google_service_account.runtime.name
   role               = "roles/iam.serviceAccountUser"
